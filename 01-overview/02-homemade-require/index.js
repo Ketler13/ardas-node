e194d27e7a41ac9c91fb09d1d215692b55d63f92
Ketler13 <oleksandr.martyshchenko@gmail.com>
2019-06-10 23:08:47 +0300

    Add hometask 1

--- 01-overview/02-homemade-require/index.js
+++ 01-overview/02-homemade-require/index.js
@@ -1,17 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function loadModule(filename, module, require) {
-  const wrappedSrc =
-    `(function(module, exports, require) {
-      ${fs.readFileSync(filename, 'utf8')}
+  const wrappedSrc = `(function(module, exports, require) {
+      ${fs.readFileSync(filename, "utf8")}
     })(module, module.exports, require);`;
   eval(wrappedSrc);
 }
 
 function _require(moduleName) {
-  console.log('Require invoked for module: ' + moduleName);
+  console.log("Require invoked for module: " + moduleName);
   const id = _require.resolve(moduleName);
-  if(_require.cache[id]) {
+  if (_require.cache[id]) {
     return _require.cache[id].exports;
   }
   const module = {
@@ -21,12 +20,12 @@ function _require(moduleName) {
   _require.cache[id] = module;
   loadModule(id, module, _require);
   return module.exports;
-};
+}
 
 _require.cache = {};
 _require.resolve = function(moduleName) {
   return moduleName;
-}
+};
 
-const revealingModule = _require('./01-revealing-module-pattern.js');
+const revealingModule = _require("./requiredModule.js");
 console.log(revealingModule);
